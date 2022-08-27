5a5c5ddd0f090569b0e0a29319c59f06475c0a2a
Irakli <i.botchori@gmail.com>
2022-08-27 15:25:18 +0400

    wip: add content on EmployeeInfo page

--- src/pages/EmployeeInfo/EmployeeInfo.jsx
+++ src/pages/EmployeeInfo/EmployeeInfo.jsx
@@ -1,7 +1,35 @@
+import { GoBackIcon, Logo } from 'assets'
 import React from 'react'
 
 const EmployeeInfo = () => {
-  return <div>EmployeeInfo</div>
+  return (
+    <div className='bg-gray-100 w-full h-screen flex flex-col justify-between'>
+      {/* Go Back Button */}
+      <div className='pl-8 pt-5 hidden sm:block'>
+        <img src={GoBackIcon} alt='Go Back Button' className='w-10' />
+      </div>
+      {/* Title */}
+      <div className='flex w-full justify-center gap-16 pb-5 font-semibold '>
+        <div>
+          <p className=''>თანამშრომლის ინფო</p>
+          <div className='w-[80%] bg-gray-600 h-[3px] m-auto mt-2'></div>
+        </div>
+        <div>
+          <p>ლეპტოპის მახასიათებლები</p>
+          <div className='w-[80%] bg-gray-600 h-[3px] m-auto mt-2'></div>
+        </div>
+      </div>
+      {/* Main Content */}
+      <div className='flex justify-center h-full'>
+        <form className=' w-full sm:w-[60%]  bg-white rounded-xl '></form>
+      </div>
+
+      {/* Redberry Logo */}
+      <div className=' m-auto py-10 hidden sm:block '>
+        <img src={Logo} alt='Logo' className='w-14' />
+      </div>
+    </div>
+  )
 }
 
 export default EmployeeInfo
