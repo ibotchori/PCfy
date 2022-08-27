91672af95d9e552a3d3863ff8a98e0212f62859f
Irakli <i.botchori@gmail.com>
2022-08-27 13:08:54 +0400

    style: add content on Landing page

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as LandingImage } from './images/LandingImage.png'
+export { default as LandingImageMob } from './images/LandingImageMob.png'
+export { default as Redberry } from './images/Redberry.png'
+export { default as Logo } from './images/Logo.png'
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,41 @@
+import { LandingImage, LandingImageMob, Redberry } from 'assets'
 import React from 'react'
 
 const Landing = () => {
-  return <div>Landing</div>
+  return (
+    <div className='w-full h-screen  flex flex-col justify-between items-center'>
+      {/* Redberry Title */}
+      <div className='pt-14'>
+        <img src={Redberry} alt='Redberry' />
+      </div>
+      {/* Main Image */}
+      <div>
+        <img
+          className='hidden sm:block w-[95%] pl-24 pb-10'
+          src={LandingImage}
+          alt='LandingImg'
+        />
+        <img
+          className='block sm:hidden'
+          src={LandingImageMob}
+          alt='LandingImg-mob'
+        />
+      </div>
+      {/* Buttons */}
+      <div className='flex flex-col gap-3 sm:gap-6 pb-7 sm:pb-24'>
+        <button
+          className={`px-16 py-4 sm:py-3 bg-[#62A1EB]   rounded-lg  text-center  text-lg  tracking-widest text-white hover:text-gray-100`}
+        >
+          ჩანაწერის დამატება
+        </button>
+        <button
+          className={`px-16 py-4 sm:py-3 bg-[#62A1EB]   rounded-lg  text-center  text-lg  tracking-widest text-white hover:text-gray-100`}
+        >
+          ჩანაწერების სია
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
